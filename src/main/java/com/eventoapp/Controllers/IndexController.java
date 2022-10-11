0d6be2ecf68e6f0b15d723b570f4aa26740335fd
rlcarneiro97 <rafaeldelima97@hotmail.com>
2022-10-11 19:55:13 -0300

    corrigindo erro do favicon.ico

--- src/main/java/com/eventoapp/Controllers/IndexController.java
+++ src/main/java/com/eventoapp/Controllers/IndexController.java
@@ -1,6 +1,8 @@
 package com.eventoapp.Controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -8,6 +10,11 @@ public class IndexController {
     @RequestMapping("/")
     public String index(){
         return "redirect:/eventos";
+    }
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {
     }
 
 }
